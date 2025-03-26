362cbed2e40ed0a1436bc093e7747807b6392db7
Miriam Fuchs <fuchsmiriam@yahoo.com>
2025-03-26 12:47:04 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from "./core/navbar/navbar.component";
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

da40bdb0a27d9224f43d5c2453c1b39420bcc1b1
Miriam Fuchs <fuchsmiriam@yahoo.com>
2025-04-11 15:40:16 +0200

    customer details added

--- src/app/customers/customer-detail/customer-detail.component.ts
+++ src/app/customers/customer-detail/customer-detail.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-customer-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './customer-detail.component.html',
-  styleUrl: './customer-detail.component.scss'
+  styleUrl: './customer-detail.component.scss',
 })
-export class CustomerDetailComponent {
+export class CustomerDetailComponent implements OnInit {
+  customer$!: Observable<any>;
 
+  constructor(
+    private route: ActivatedRoute,
+    private customerService: CustomerService
+  ){}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.customer$ = this.customerService.getCustomer(id);
+    }
+  }
 }
